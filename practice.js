740125c53ae8e380fe9ebe20da979a90b470cb8b
Jihyun_Kim <jihyun3313@daum.net>
2024-06-21 02:02:27 +0900

    docs: Add JS/클로저

--- practice.js
+++ practice.js
@@ -1,23 +1,8 @@
-const originalMatrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+function Component() {
+  const [state, setState] = useState();
 
-const rows = originalMatrix.length;
-const cols = originalMatrix[0].length;
-
-const transposedMatrix = [];
-
-for (let i = 0; i < cols; i++) {
-  transposedMatrix[i] = [];
-  for (let j = 0; j < rows; j++) {
-    transposedMatrix[i][j] = originalMatrix[j][i];
+  function handleClick() {
+    setState((prev) => prev + 1);
   }
+  //...
 }
-
-console.log(originalMatrix);
-// [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]
-
-console.log(transposedMatrix);
-// [ [ 1, 4, 7 ], [ 2, 5, 8 ], [ 3, 6, 9 ] ]
